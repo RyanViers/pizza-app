504180919ca1aebdb1c058c12ab530caa45c64d6
RyanViers <ryanviersiv@hotmail.com>
2023-04-07 13:23:48 -0400

    first

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styles: [``],
+  standalone: true,
+  imports: [IonicModule],
+})
+export class HomePage {
+  constructor() {}
+}
