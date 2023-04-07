d6da1eb51de4b0e2c78673610b5854a1c8dd4707
RyanViers <ryanviersiv@hotmail.com>
2023-04-07 13:28:46 -0400

    second

---
+++ src/app/modules/administrator/admin-root/admin-root.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-root',
+  templateUrl: './admin-root.component.html',
+  styleUrls: ['./admin-root.component.scss'],
+})
+export class AdminRootComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
