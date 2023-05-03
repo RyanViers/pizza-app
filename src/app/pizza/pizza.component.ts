4e40b0a70502ba5774ee9b802107366daf9cab04
Andrew Haselden <andrewhaselden@headcount.company>
2023-05-03 16:59:57 -0400

    latest

--- src/app/pizza/pizza.component.ts
+++ src/app/pizza/pizza.component.ts
@@ -16,11 +16,11 @@ import { PizzaService } from "./pizza.service";
                 <div class="overflow-hidden rounded-full bg-gray-200">
                     <div class="h-2 rounded-full bg-indigo-600" [ngClass]="getWidth()"></div>
                 </div>
-                <div class="mt-6 hidden grid-cols-4 text-sm font-medium text-gray-600 sm:grid">
-                    <div (click)="goToSection(0)" [ngClass]="getColor(0)" class="cursor-pointer">Base</div>
-                    <div (click)="goToSection(1)" [ngClass]="getColor(1)" class="text-center cursor-pointer">Cheese</div>
-                    <div (click)="goToSection(2)" [ngClass]="getColor(2)" class="text-center cursor-pointer">Meats</div>
-                    <div (click)="goToSection(3)" [ngClass]="getColor(3)" class="text-right cursor-pointer">Veggies</div>
+                <div class="mt-6 hidden grid-cols-4 text-sm font-medium text-dark-tint sm:grid">
+                    <div (click)="goToSection(0)" [ngClass]="getColor(0)" class="cursor-pointer hover:text-gray-600">Base</div>
+                    <div (click)="goToSection(1)" [ngClass]="getColor(1)" class="text-center cursor-pointer hover:text-gray-600">Cheese</div>
+                    <div (click)="goToSection(2)" [ngClass]="getColor(2)" class="text-center cursor-pointer hover:text-gray-600">Meats</div>
+                    <div (click)="goToSection(3)" [ngClass]="getColor(3)" class="text-right cursor-pointer hover:text-gray-600">Veggies</div>
                 </div>
                 </div>
             </div>
@@ -36,7 +36,22 @@ import { PizzaService } from "./pizza.service";
         
         <div class="w-full flex justify-between items-center">
             <button (click)="back()" type="button" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Back</button>
-            <button (click)="addToOrder()" type="button" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add to Order</button>
+            <div class="flex flex-row space-x-6 items-center justify-center">
+                <div class="flex flex-row space-x-2 items-center justify-center">
+                    <button type="button" class="rounded-full bg-light-tint p-2 text-dark shadow-sm hover:bg-light focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-light">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15" />
+                        </svg>                  
+                    </button>
+                    <ion-label class="text-lg text-dark">1</ion-label>
+                    <button type="button" class="rounded-full bg-light-tint p-2 text-dark shadow-sm hover:bg-light focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                        </svg>
+                    </button>
+                </div>
+                <button (click)="addToOrder()" type="button" class="rounded-md bg-light-tint px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-light focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add to Order</button>
+            </div>
             <button (click)="next()" type="button" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Next</button>
         </div>
 
@@ -83,7 +98,7 @@ export class PizzaComponent {
      * @returns 
      */
     getColor(precedence: number) {
-        return this.pizza.getSectionPrecedence(this.pizza.getSection()) >= precedence ? 'text-indigo-600' : '';
+        return this.pizza.getSectionPrecedence(this.pizza.getSection()) >= precedence ? '!text-indigo-600' : '';
     }
 
     /**
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        'max': 'var(--ion-max-width)'
+      },
+      colors: {
+        'primary': 'var(--ion-color-primary)',
+        'primary-tint': 'var(--ion-color-primary-tint)',
+        'primary-shade': 'var(--ion-color-primary-shade)',
+        'secondary': 'var(--ion-color-secondary)',
+        'secondary-tint': 'var(--ion-color-secondary-tint)',
+        'secondary-shade': 'var(--ion-color-secondary-shade)',
+        'tertiary': 'var(--ion-color-tertiary)',
+        'success': 'var(--ion-color-success)',
+        'warning': 'var(--ion-color-warning)',
+        'danger': 'var(--ion-color-danger)',
+        'light': 'var(--ion-color-light)',
+        'light-tint': 'var(--ion-color-light-tint)',
+        'light-shade': 'var(--ion-color-light-shade)',
+        'medium': 'var(--ion-color-medium)',
+        'medium-shade': 'var(--ion-color-medium-shade)',
+        'medium-tint': 'var(--ion-color-medium-tint)',
+        'dark': 'var(--ion-color-dark)',
+        'dark-tint': 'var(--ion-color-dark-tint)',
+        'dark-shade': 'var(--ion-color-dark-shade)',
+        'black': 'var(--ion-color-black)',
+        'white': 'var(--ion-color-white)',
+      }
+    },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [],
 };
