7cecd367e4257ea8b9a36a55040a57c6efa3576c
RyanViers <ryanviersiv@hotmail.com>
2023-07-26 20:16:06 -0400

    desktop wed

--- amplify/backend/custom/AppsyncResolvers/resolver-maps/queries.ts
+++ amplify/backend/custom/AppsyncResolvers/resolver-maps/queries.ts
@@ -1,4 +1,21 @@
-export const queryMap = new Map<string, {requestTemplate: string, responseTemplate: string, fieldName: string}>([    
-    ["ListOrdersByUser", {requestTemplate: 'listOrdersByUser.req.vtl', responseTemplate: 'listOrdersByUser.res.vtl', fieldName: 'listOrdersByUser'}],
-    ["GetOrder", {requestTemplate: 'getOrder.req.vtl', responseTemplate: 'getOrder.res.vtl', fieldName: 'getOrder'}]
+export const queryMap = new Map<
+  string,
+  { requestTemplate: string; responseTemplate: string; fieldName: string }
+>([
+  [
+    'ListOrdersByUser',
+    {
+      requestTemplate: 'listOrdersByUser.req.vtl',
+      responseTemplate: 'listOrdersByUser.res.vtl',
+      fieldName: 'listOrdersByUser',
+    },
+  ],
+  [
+    'GetOrder',
+    {
+      requestTemplate: 'getOrder.req.vtl',
+      responseTemplate: 'getOrder.res.vtl',
+      fieldName: 'getOrder',
+    },
+  ],
 ]);
