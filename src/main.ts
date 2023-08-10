43f85e0f9d63aa0dadba95489070085f895cab51
Andrew Haselden <andrewhaselden@headcount.company>
2023-08-10 13:07:06 -0400

    latest

--- src/main.ts
+++ src/main.ts
@@ -65,6 +65,10 @@ bootstrapApplication(AppComponent, {
     provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
     })
 ],
 }).catch((e) => console.error(e));
